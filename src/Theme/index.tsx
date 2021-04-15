2fc3aac7756300f7776105ab2d7ae651c628722b
Ian Macalinao <github@igm.pub>
2021-04-15 05:25:31 -0500

    everything inter

--- src/Theme/index.tsx
+++ src/Theme/index.tsx
@@ -156,7 +156,7 @@ export const ThemedBackground = styled.div<{ backgroundColor: string; endColor?:
 
 export const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&display=swap');
-  html { font-family: 'Inter', sans-serif; }
+  * { font-family: 'Inter', sans-serif; }
   
   html,
   body {
