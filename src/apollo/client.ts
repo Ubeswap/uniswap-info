f9a275a0f8e68a0a3176f4ba3792561e343dda0e
Brian Li <brnli7@gmail.com>
2021-09-11 08:12:43 -0700

    Fix subgraph

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/ubeswap/ubeswap',
+    uri: 'https://api.thegraph.com/subgraphs/id/QmRSND9sqqzfutbMgCVJtW12mcsKBK5UCFqXh867AE9GyJ',
   }),
   cache: new InMemoryCache(),
 })
