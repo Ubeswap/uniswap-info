2e66b1b038447f8a3fef07a44236e36d71384d57
Brian Li <brnli7@gmail.com>
2021-09-13 05:30:52 -0700

    Use fixed subgraph

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/id/QmRSND9sqqzfutbMgCVJtW12mcsKBK5UCFqXh867AE9GyJ',
+    uri: 'https://api.thegraph.com/subgraphs/name/briungri/ubeswap-backup',
   }),
   cache: new InMemoryCache(),
 })
