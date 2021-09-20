da799a34bb00697943b605aa9f3d0211d03f9c57
Brian Li <brnli7@gmail.com>
2021-09-20 08:10:25 -0700

    Point to Ubeswap gql endpoint

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/briungri/ubeswap-backup',
+    uri: 'https://api.thegraph.com/subgraphs/name/ubeswap/ubeswap',
   }),
   cache: new InMemoryCache(),
 })
